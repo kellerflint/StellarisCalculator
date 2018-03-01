66ab0a5ba2952c89c7710a491b28b260f6aadb17
Keller Flint <kellerflint@Kellers-MacBook-Air.local>
2018-02-28 20:28:22 -0800

    Added function to calculate required system science

--- stellarisScience.py
+++ stellarisScience.py
@@ -3,7 +3,6 @@
 # pre: does NOT include stored science in monthly progress
 import sys
 
-
 def monthsCost():
     print("This calculates the combined cost in months of all given research.")
 
@@ -145,7 +144,7 @@ def removeSystem():
     socTotal = (socBaseGain - (socSciVal * pacifistMod)) * socSciMod
     engTotal = (engBaseGain - (engSciVal * pacifistMod)) * engSciMod
 
-    # Adds new system to penalty
+    # Removes system from penalty
     penalty -= systemCost
 
     newCost = ((phyBaseCost * penalty)/phyTotal) +\
@@ -164,6 +163,92 @@ def removeSystem():
         print(f'\nRemoving this system will INCREASE total research time by {difference}')
 
 
+#Required System Science - Amount of science need in a system to break even on aggregate research rate (measured in months)
+#NOTE This is probably gonna provide a low estimate (because ignoring non-generic bonuses and integer base costs).
+#I think this is fine since the math is providing the break even point. But ideally you're gaining not
+#just breaking even anyways. (at least for my science min/maxing)
+def reqSysSci():
+    print("Will return the minimum amount of science a system requires to break even with its research penalties.")
+    #Pacifist modifier
+    pacifistDegree = int(input("Enter your degree of PACIFISM (0-Not pacifist 1-pacifist 2-Fanatic pacifist): "))
+
+    pacifistMod = 1 + (pacifistDegree * .05)
+
+    #Average base cost
+    avgBaseCost = float(input("\nEnter the AVERAGE BASE COST of your current (or typical) research: "))
+
+    #Science modifiers (combined [generic bonus only] (what about the constant bonuses? - none significant?)
+    genSciMod = float(input("\nEnter your total GENERIC SCIENCE MODIFIER (int): "))
+
+    genSciMod *= .01
+    genSciMod += 1
+
+    #Penalty data
+    systemCost = .02
+    planetCost = .05
+
+    sigSystems = int(input("\nEnter your total number of SYSTEMS: "))
+    sigPlanets = int(input("Enter your total number of PLANETS: "))
+
+    sigSystems -= 1
+    sigPlanets -= 1
+
+    # Base science gain
+    basePerMonth = [1,1,1]
+    basePerMonth[0] = float(input("\nEnter your BASE PHYSICS/month: "))
+    basePerMonth[1] = float(input("Enter your BASE SOCIETY/month: "))
+    basePerMonth[2] = float(input("Entery your BASE ENGINEERING/month: "))
+
+    difference = 1;
+
+    reqSciPerSys = 1;
+
+    while difference > 0:
+
+        #############
+        ###orgCost###
+        #############
+
+        #calculate total research penalty
+        penalty = 1 + ((planetCost * sigPlanets) +\
+                  (systemCost * sigSystems))
+
+        #calculate actual research cost
+        avgTechCost = penalty * avgBaseCost
+
+        #Approximate combined average research
+        avgResearch = ((1/basePerMonth[0]) * genSciMod) +\
+                      ((1/basePerMonth[1]) * genSciMod) +\
+                      ((1/basePerMonth[2]) * genSciMod)
+
+        #originalCost is in months (average to complete level of research)
+        originalCost = avgTechCost * avgResearch #avgResearch is < 1
+
+        #############
+        ###newCost###
+        #############
+
+        #Additional system incurs system science penalty
+        penalty += systemCost;
+
+        avgTechCost = penalty * avgBaseCost
+
+
+        avgResearch =  (1/(basePerMonth[0] + ((reqSciPerSys/3) * pacifistMod))*genSciMod) +\
+                       (1/(basePerMonth[1] + ((reqSciPerSys/3) * pacifistMod))*genSciMod) +\
+                       (1/(basePerMonth[2] + ((reqSciPerSys/3) * pacifistMod))*genSciMod)
+
+        newCost = avgTechCost * avgResearch
+
+        difference = newCost - originalCost
+
+        if difference > 0:
+            reqSciPerSys += 1
+
+    print(f'\nNew systems must have at a minimum of {reqSciPerSys} science to break even after tech penalty.')
+
+
+
 def main():
 
     print("This program provides some tools for Stellaris science calculations.")
@@ -173,16 +258,19 @@ def main():
     while option != 0:
 
         try:
-            option = int(input("\n0) exit 1) total cost in months 2) Add a system 3) remove a system: "))
+            option = int(input("0) exit \n1) total cost in months \n2) Add a system " +
+                               "\n3) remove a system\n4) Find Required System Science\n"))
 
-            if option == 1:
+            if option == 0:
+                print("Exiting...")
+            elif option == 1:
                 monthsCost()
             elif option == 2:
                 addSystem()
             elif option == 3:
                 removeSystem()
-            elif option == 0:
-                print("Exiting...")
+            elif option == 4:
+                reqSysSci()
             else:
                 print("No valid option selected.")
 
