0fa9ff88a894b9376b7339ae8e4b63e3cb20a7d4
kellerflint <kflint0068@gmail.com>
2018-03-28 20:19:54 -0700

    Started functional decomposition conversion

--- scripts.js
+++ scripts.js
@@ -2,58 +2,35 @@
 var systemCost = .02;
 var planetCost = .05;
 
+// Globals
+var pacifistMod = 0;
+
+var genSciMod = 0;
+
+var basePHY = 0;
+var basePHY = 0;
+var basePHY = 0;
+
+var sigSystems = 0;
+var sigPlanets = 0;
+
 function main() {
+  update();
   sysSciReq();
   displayResearchRate();
   clearGains();
 }
 
-function dropdown() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+function update() {
+  pacifistMod = document.getElementById("i_pacifist").value;
+  genSciMod = document.getElementById("i_genSciMod").value;
 
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-  }
-}
+  basePHY = document.getElementById("i_basePHY").value;
+  baseSOC = document.getElementById("i_baseSOC").value;
+  baseENG = document.getElementById("i_baseENG").value;
 
-function clearGains() {
-  document.getElementById("averageGain").innerHTML = "";
-  document.getElementById("physicsGain").innerHTML = "";
-  document.getElementById("societyGain").innerHTML = "";
-  document.getElementById("engineeringGain").innerHTML = "";
-}
-
-function clearAll() {
-  document.getElementById("averageGain").innerHTML = "";
-  document.getElementById("physicsGain").innerHTML = "";
-  document.getElementById("societyGain").innerHTML = "";
-  document.getElementById("engineeringGain").innerHTML = "";
-
-  document.getElementById("reqSciPerSys").innerHTML = "Average System Science: ";
-  document.getElementById("avgResearchRate").innerHTML = "Average Research Rate: ";
-  document.getElementById("phyResearchRate").innerHTML = "&ensp; &ensp; Physics Rate: ";
-  document.getElementById("socResearchRate").innerHTML = "&ensp; &ensp; Society Rate: ";
-  document.getElementById("engResearchRate").innerHTML = "&ensp; &ensp; Engineering Rate: ";
-}
-
-function sysSciReq() {
-
-  // Retrieve form values
-  var pacifistMod = document.getElementById("i_pacifist").value;
-  var avgBaseCost = 1000;
-  var genSciMod = document.getElementById("i_genSciMod").value;
-
-  var sigSystems = document.getElementById("i_systemNum").value;
-  var sigPlanets = document.getElementById("i_planetNum").value;
-
-  var basePHY = document.getElementById("i_basePHY").value;
-  var baseSOC = document.getElementById("i_baseSOC").value;
-  var baseENG = document.getElementById("i_baseENG").value;
+  sigSystems = document.getElementById("i_systemNum").value;
+  sigPlanets = document.getElementById("i_planetNum").value;
 
   // To integer (find a less stupid way to do this)
   basePHY = + basePHY;
@@ -64,54 +41,34 @@ function sysSciReq() {
   pacifistMod = 1 + (pacifistMod * .05);
   genSciMod = 1 + (genSciMod * .01);
 
+  // Remove free planet and system
   sigSystems = sigSystems - 1;
   sigPlanets = sigPlanets - 1;
 
+}
+
+function sysSciReq() {
+
   var difference = 1;
+
+  var originalRates = researchRate(basePHY, baseSOC, baseENG, 0);
 
   var reqSciPerSys = 1;
 
   // Calcuations
   while (difference > 0) {
 
-    // ---Original cost---
+    var originalRates = researchRate(physics, society, engineering, 0);
+    document.getElementById("output5").innerHTML = "originalRates " + originalRates;
 
-    // Calculate total research penalty
-    var penalty = 1 + ((planetCost * sigPlanets) +
-                       (systemCost * sigSystems));
-    // Calculate actual research cost
-    var avgTechCost = penalty * avgBaseCost;
+    var physics = (parseFloat(document.getElementById("i_addPHY").value) * pacifistMod) + parseFloat(document.getElementById("i_basePHY").value);
+    var society = (parseFloat(document.getElementById("i_addSOC").value) * pacifistMod) + parseFloat(document.getElementById("i_baseSOC").value);
+    var engineering = (parseFloat(document.getElementById("i_addENG").value) * pacifistMod) + parseFloat(document.getElementById("i_baseENG").value);
 
-    // Approximate combined average research
-    var avgResearch = (1/(basePHY * genSciMod)) +
-                      (1/(baseSOC * genSciMod)) +
-                      (1/(baseENG * genSciMod));
 
-    // OriginalCost is in months (average to complete level of research)
-    var originalCost = avgTechCost * avgResearch; // where avgResearch < 1;
-    // ---New cost---
+    var newRates = researchRate(physics, society, engineering, 1);
 
-    // Additional system incurs system science penalty
-    penalty = penalty + systemCost;
-
-    avgTechCost = penalty * avgBaseCost;
-
-    avgResearch =  (1/((basePHY + ((reqSciPerSys/3) * pacifistMod))*genSciMod)) +
-                   (1/((baseSOC + ((reqSciPerSys/3) * pacifistMod))*genSciMod)) +
-                   (1/((baseENG + ((reqSciPerSys/3) * pacifistMod))*genSciMod));
-
-    var newCost = avgTechCost * avgResearch;
-
-    // Test code
-    document.getElementById("output1").innerHTML = "reqSciPerSys/3: " + ((reqSciPerSys/3));
-    document.getElementById("output2").innerHTML = "oldcost: " + originalCost;
-    document.getElementById("output3").innerHTML = "newcost: " + newCost;
-    document.getElementById("output4").innerHTML = "penalty: " + penalty;
-    document.getElementById("output5").innerHTML = "final physics: " + ((basePHY + ((reqSciPerSys/3) * pacifistMod))*genSciMod);
-
-    difference = newCost - originalCost;
-
-    document.getElementById("output6").innerHTML = "diff " + difference;
+    difference = originalRates[0] - newRates[0];
 
     if (difference > 0) {
       reqSciPerSys = reqSciPerSys + 1;
@@ -133,7 +90,8 @@ function displayResearchRate() {
   document.getElementById("engResearchRate").innerHTML = "&ensp; &ensp; Engineering Rate: " + (Math.round(rates[3] * 10)/10);
 }
 
-// isChange 1 for adding a system, 0 for not a change calculation and -1 for removing a system
+// isChange = x where x is number of added systems, 0 for no change calculation and -x for removing a system
+// physics, society and engineering are the total values not just the base values
 function researchRate(physics, society, engineering, isChange) {
   var genSciMod = document.getElementById("i_genSciMod").value;
 
@@ -303,4 +261,41 @@ function removeSystem() {
     document.getElementById("engineeringGain").innerHTML = " (" + Math.round(difference * 10)/10 + ")";
   }
 
+}
+
+// Clear functions
+
+function clearGains() {
+  document.getElementById("averageGain").innerHTML = "";
+  document.getElementById("physicsGain").innerHTML = "";
+  document.getElementById("societyGain").innerHTML = "";
+  document.getElementById("engineeringGain").innerHTML = "";
+}
+
+function clearAll() {
+  document.getElementById("averageGain").innerHTML = "";
+  document.getElementById("physicsGain").innerHTML = "";
+  document.getElementById("societyGain").innerHTML = "";
+  document.getElementById("engineeringGain").innerHTML = "";
+
+  document.getElementById("reqSciPerSys").innerHTML = "Average System Science: ";
+  document.getElementById("avgResearchRate").innerHTML = "Average Research Rate: ";
+  document.getElementById("phyResearchRate").innerHTML = "&ensp; &ensp; Physics Rate: ";
+  document.getElementById("socResearchRate").innerHTML = "&ensp; &ensp; Society Rate: ";
+  document.getElementById("engResearchRate").innerHTML = "&ensp; &ensp; Engineering Rate: ";
+}
+
+// Dropdown actions
+
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
 }
